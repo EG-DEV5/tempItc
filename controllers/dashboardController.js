e7a6caf25d14d288c3899a6a81b7bb259f34b4b1
ahmedbasher-2 <egypt.team.dev12@gmail.com>
2023-03-14 14:12:13 +0200

    vehicleViolationsQuery fix

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,7 @@
 /** @format */
-const CustomError = require('../errors');
-const { StatusCodes } = require('http-status-codes');
-const { faker } = require('@faker-js/faker');
+const CustomError = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const { faker } = require('@faker-js/faker')
 const {
   getUserVehiclesFMS,
   harshAccelerationQuery,
@@ -11,110 +11,143 @@ const {
   IsOverSpeedQuery,
   seatBeltQuery,
   vehicleViolationsQuery,
-  nightDriveQuery
-} = require('../helpers/helper');
+  nightDriveQuery,
+} = require('../helpers/helper')
 
 const harshAcceleration = async (req, res) => {
   // const vhs = await getusersVhs();
-  let { strDate,endDate,vehIDs } = req.query;
-  if ( vehIDs || vehIDs == 'string') {
-    vehIDs = JSON.parse(vehIDs);
+  let { strDate, endDate, vehIDs } = req.query
+  if (vehIDs || vehIDs == 'string') {
+    vehIDs = JSON.parse(vehIDs)
   }
-  let result = await harshAccelerationQuery(strDate,endDate,vehIDs);
-  if(result.length == 0 ){
-    throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+  let result = await harshAccelerationQuery(strDate, endDate, vehIDs)
+  if (result.length == 0) {
+    throw new CustomError.BadRequestError(
+      '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+    )
   }
-    res.status(200).json({ result });
-};
+  res.status(200).json({ result })
+}
 const HarshBreaking = async (req, res) => {
   // const data = await getusersVhs();
-  let { strDate,endDate,vehIDs } = req.query;
-  if ( vehIDs || vehIDs == 'string') {
-    vehIDs = JSON.parse(vehIDs);
+  let { strDate, endDate, vehIDs } = req.query
+  if (vehIDs || vehIDs == 'string') {
+    vehIDs = JSON.parse(vehIDs)
   }
-  let result = await HarshBreakingQuery(strDate,endDate,vehIDs);
-  if(result.length == 0 ){
-    throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+  let result = await HarshBreakingQuery(strDate, endDate, vehIDs)
+  if (result.length == 0) {
+    throw new CustomError.BadRequestError(
+      '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+    )
   }
-  res.status(200).json({ result });
-};
+  res.status(200).json({ result })
+}
 const IsOverSpeed = async (req, res) => {
   // const data = await getusersVhs();
-   let { strDate,endDate,vehIDs } = req.query;
-   if ( vehIDs || vehIDs == 'string') {
-    vehIDs = JSON.parse(vehIDs);
+  let { strDate, endDate, vehIDs } = req.query
+  if (vehIDs || vehIDs == 'string') {
+    vehIDs = JSON.parse(vehIDs)
   }
-  let result = await IsOverSpeedQuery(strDate,endDate,vehIDs);
-  if(result.length == 0 ){
-    throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+  let result = await IsOverSpeedQuery(strDate, endDate, vehIDs)
+  if (result.length == 0) {
+    throw new CustomError.BadRequestError(
+      '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+    )
   }
-  res.status(200).json({ result });
-};
+  res.status(200).json({ result })
+}
 const seatBelt = async (req, res) => {
   // const vhs = await getusersVhs();
-   let { strDate,endDate,vehIDs } = req.query;
-   if ( vehIDs || vehIDs == 'string') {
-    vehIDs = JSON.parse(vehIDs);
+  let { strDate, endDate, vehIDs } = req.query
+  if (vehIDs || vehIDs == 'string') {
+    vehIDs = JSON.parse(vehIDs)
   }
-  let result = await seatBeltQuery(strDate,endDate,vehIDs );
-  if(result.length == 0 ){
-    throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+  let result = await seatBeltQuery(strDate, endDate, vehIDs)
+  if (result.length == 0) {
+    throw new CustomError.BadRequestError(
+      '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+    )
   }
   // let data = [];
-// result.forEach((element, i) => {
-//     if(data.filter(e => e?.date == element._id.date) .length > 0) {
-//         data[data.findIndex(ee => ee?.date == element._id.date)].count ++ 
-//     }else{
-//         data.push({
-//             date: element._id.date,
-//             count: 1
-//         })
-//     }
-// });
-  res.status(200).json({ result });
-};
+  // result.forEach((element, i) => {
+  //     if(data.filter(e => e?.date == element._id.date) .length > 0) {
+  //         data[data.findIndex(ee => ee?.date == element._id.date)].count ++
+  //     }else{
+  //         data.push({
+  //             date: element._id.date,
+  //             count: 1
+  //         })
+  //     }
+  // });
+  res.status(200).json({ result })
+}
 
 const mainDashboard = async (req, res) => {
   // const data = await getusersVhs();
-   let { strDate,endDate,vehIDs } = req.query;
-  let result = await mainDashboardQuery(strDate,endDate,vehIDs);
-  delete result[0]._id;
-  result[0].nightDriving = 0;
-  res.status(200).json(result[0]);
-};
+  let { strDate, endDate, vehIDs } = req.query
+  let result = await mainDashboardQuery(strDate, endDate, vehIDs)
+  delete result[0]._id
+  result[0].nightDriving = 0
+  res.status(200).json(result[0])
+}
 
-const nightDriving = async(req, res) => {
-  let { strDate,endDate,vehIDs } = req.query;
-   if ( vehIDs || vehIDs == 'string') {
-    vehIDs = JSON.parse(vehIDs);
+const nightDriving = async (req, res) => {
+  let { strDate, endDate, vehIDs } = req.query
+  if (vehIDs || vehIDs == 'string') {
+    vehIDs = JSON.parse(vehIDs)
   }
-  let result = await nightDriveQuery(strDate,endDate,vehIDs );
-  if(result.length == 0 ){
-    throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+  let result = await nightDriveQuery(strDate, endDate, vehIDs)
+  if (result.length == 0) {
+    throw new CustomError.BadRequestError(
+      '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+    )
   }
-  res.send({result})
-};
+  res.send({ result })
+}
 const sharpTurns = (req, res) => {
   res.status(200).json({
     result: [],
-  });
-};
-const vehicleViolations = async(req, res, next)=>{
-  let { strDate,endDate,vehIDs } = req.query;
+  })
+}
+const vehicleViolations = async (req, res, next) => {
+  let { strDate, endDate, vehIDs } = req.query
   try {
-    if ( vehIDs || vehIDs == 'string') {
-      vehIDs = JSON.parse(vehIDs);
+    if (vehIDs || vehIDs == 'string') {
+      vehIDs = JSON.parse(vehIDs)
     }
-    let result = await vehicleViolationsQuery(strDate, endDate,vehIDs);
-    if(result.length == 0 ){
-      throw new CustomError.BadRequestError('{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}')
+
+    let [{ result, totalViolation }] = await vehicleViolationsQuery(
+      strDate,
+      endDate,
+      vehIDs
+    )
+
+    const arr1 = result.map((el) => {
+      return el._id
+    })
+
+    const arr2 = vehIDs.filter((el) => !arr1.includes(el))
+
+    arr2.map((el) =>
+      result.unshift({
+        _id: el,
+        harshAcceleration: 0,
+        OverSpeed: 0,
+        SeatBelt: 0,
+        harshBrake: 0,
+        nightDrive: 0,
+      })
+    )
+    if (!result) {
+      throw new CustomError.BadRequestError(
+        '{"enMessage" : "there is no data in this period", "arMessage" :"لا توجد بيانات فى هذه الفترة"}'
+      )
     }
-    delete result[0]._id;
-    res.status(200).json(result[0]);
+
+    res.status(200).json({ result, totalViolation })
   } catch (error) {
     next(error)
   }
- 
 }
 
 module.exports = {
@@ -125,5 +158,5 @@ module.exports = {
   nightDriving,
   sharpTurns,
   seatBelt,
-  vehicleViolations
-};
+  vehicleViolations,
+}
