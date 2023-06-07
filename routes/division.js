bb4525136cccd3f3bc0042a342e2228757012547
EG-DEV5 <egypt.team.dev5@gmail.com>
2023-06-07 11:51:17 +0300

    feat : get Divisions Filter

--- routes/division.js
+++ routes/division.js
@@ -7,10 +7,12 @@ const {
   addDivision,
   getDivision,
   getDivisionById,
+  getDivisionsFilter,
 } = require('../controllers/divisionController')
 const { authenticateUser } = require('../middleware/authentication')
 
 router.post('/addDivision', authenticateUser, addDivision)
 router.get('/getDivision', authenticateUser, getDivision)
+router.get('/getDivisionsFilter', authenticateUser, getDivisionsFilter)
 router.get('/getDivision/:id', authenticateUser, getDivisionById)
 module.exports = router
